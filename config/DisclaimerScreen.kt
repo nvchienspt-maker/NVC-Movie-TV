70f8995a04076b660859737c9320abf9419e4bdf
NVC <nvchien.spt@gmail.com>
2026-08-16 10:42:45 +0700

    Add disclaimer and terms of use to DisclaimerScreen

--- config/DisclaimerScreen.kt
+++ config/DisclaimerScreen.kt
@@ -1,3 +1,5 @@
+TUYÊN BỐ TỪ CHỐI TRÁCH NHIỆM VÀ ĐIỀU KHOẢN SỬ DỤNG:
+Chào mừng bạn đến với Ứng dụng NVC Movie:
 1. NVC Movie là ứng dụng tổng hợp và hiển thị dữ liệu qua API có sẵn và công khai trên Internet.
 2. Chúng tôi không lưu trữ, không lưu bản sao và không tải lên bất kỳ nội dung phương tiện nào lên máy chủ của mình.
 3. Trách nhiệm về bản quyền nội dung thuộc về các trang web lưu trữ máy chủ của bên thứ ba.
