6c8cd6b9f70f044f30061f21475fb0ed7be32fdf
NVC <nvchien.spt@gmail.com>
2026-08-16 08:46:07 +0700

    Update DisclaimerScreen with version details
    Added information about two versions of the application.

--- config/DisclaimerScreen.kt
+++ config/DisclaimerScreen.kt
@@ -2,4 +2,7 @@
 2. Chúng tôi không lưu trữ, không lưu bản sao và không tải lên bất kỳ nội dung phương tiện nào lên máy chủ của mình.
 3. Trách nhiệm về bản quyền nội dung thuộc về các trang web lưu trữ máy chủ của bên thứ ba.
 4. Bằng việc bấm "Đồng ý", bạn hiểu rõ và chấp nhận các điều khoản trên khi sử dụng ứng dụng này.
+  => NVC SPACES CUNG CẤP 2 PHIÊN BẢN: NVC MOVIE VÀ NVC PHIM:
+      - NVC Phim: 5687393
+      - NVC Movie: 3422701
                          
